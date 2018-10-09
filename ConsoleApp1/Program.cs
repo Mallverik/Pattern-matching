6178bc65a288e6eb82d6f0af5bdf3d113fb45d45
Mallverik <42370162+Mallverik@users.noreply.github.com>
2018-10-09 19:03:33 +0300

    Task Done

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    class FirstException : Exception
+    {
+        public FirstException(string message)
+        {
+        }
+    }
+    class SecondException : Exception
+    {
+        public SecondException(string message)
+        {
+        }
+    }
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            string EnterMessage;
+            try
+            {
+                Console.WriteLine("Enter your message");
+                EnterMessage = Console.ReadLine();
+                if (EnterMessage == "One")
+                {
+                    throw new FirstException("Used First Exception");
+                }
+                else if (EnterMessage =="Two")
+                {
+                    throw new SecondException("Used Second Exception");
+                }
+            }
+            catch (Exception e)
+            {
+                HandleException(e);
+            }
+            Console.Read();
+        }
+        
+        public static void HandleException(Exception e)
+        {
+                switch (e)
+            {
+                case FirstException First:
+                    Console.WriteLine(First.Message);
+                    break;
+                case SecondException Second:
+                    Console.WriteLine(Second.Message);
+                    break;
+            }
+        }
+    }
+}
+
